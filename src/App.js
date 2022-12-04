e8c14b9b9fe015130224f45dc9b3c46876ab857d
Raunaq Naidu <raunaqnaidu@fb.com>
2022-12-03 22:05:42 -0800

    Pages rendering with routing

--- src/App.js
+++ src/App.js
@@ -4,14 +4,10 @@ import { Landing , Login} from './components';
 
 const App = () => {
   return (
-    <div>
-<Router>
   <Routes>
-    <Route exact path="/" element={Landing}/>
-    <Route exact path="/login" element={Login}/>
+    <Route  path="/" element={<Landing/>}/>
+    <Route  path="/login" element={<Login/>}/>
   </Routes>
-</Router>
-</div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import './fonts/Cyberpunk.ttf';
 import './fonts/Dirtchunk.ttf';
@@ -7,7 +7,11 @@ import App from "./App";
 import './index.css';
 
 import { BrowserRouter } from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById("root"))
+
 ReactDOM.render(
-<div>dfgdf </div>
-);
+    <StrictMode>
+<BrowserRouter>
+    <App />
+</BrowserRouter>
+</StrictMode>
+, document.getElementById("root"));
